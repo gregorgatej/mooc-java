10c25d6d709ae926be8cab6ca19c1be07fd0e288
Gregor Gatej <gregor.gatej@gmail.com>
2026-04-04 12:08:00 +0200

    feat: add Book class

---
+++ part-4/2-objects-in-a-list/books/src/Book.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class Book {
+    private String title;
+    private int pages;
+    private Integer publicationYear;
+
+    public Book(String title, int pages, int publicationYear) {
+        this.title = title;
+        this.pages = pages;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public int getPages() {
+        return this.pages;
+    }
+
+    public int getPublicationYear() {
+        return this.publicationYear;
+    }
+
+    public String toString() {
+        return getTitle() + ", " + getPages() + ", " + getPublicationYear();
+    }
+}

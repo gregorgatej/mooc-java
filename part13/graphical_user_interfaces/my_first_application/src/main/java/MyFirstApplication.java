8d8c91f745e635ecb06315c302a8a543da09145b
Gregor Gatej <gregor.gatej@gmail.com>
2026-04-11 11:32:24 +0200

    feat: add FlowPane and Button

--- part13/graphical_user_interfaces/my_first_application/src/main/java/MyFirstApplication.java
+++ part13/graphical_user_interfaces/my_first_application/src/main/java/MyFirstApplication.java
@@ -1,11 +1,23 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.FlowPane;
 
 public class MyFirstApplication extends Application {
 
     @Override
     public void start(Stage window) {
         window.setTitle("Hello!");
+
+        Button button = new Button("This is a button");
+
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+
+        Scene scene = new Scene(componentGroup);
+
+        window.setScene(scene);
         window.show();
     }
 
